be65f08f53a4541406808c17373c50b027ce8680
Kai Lukowiak <kailukowiak@gmail.com>
2019-06-09 21:10:55 -0600

    Init for KFP in Julia

---
+++ kfp_nn_model.jl
@@ -0,0 +1,65 @@
+using ImageView
+using CSV
+using Flux
+using Flux: @epochs
+using LinearAlgebra
+using DataFrames
+using CuArrays
+using CUDAnative
+using Statistics
+using ProgressBars
+
+train = CSV.read("data/training.csv") |> DataFrame
+
+describe(train)
+## Lots of missing values
+
+
+
+function missing_by_col(x)
+    sum(ismissing.(x))
+end
+
+
+colwise(missing_by_col, train)
+
+function replace_missing(df)
+    for col in names(df)
+        col_mean = mean(skipmissing(df[col]))
+        df[ismissing.(df[col]), col] = col_mean
+        println(col_mean)
+    end
+end
+
+replace_missing(train)
+colwise(missing_by_col, train)
+
+## Fixed missing
+X = train[:Image]
+delete!(train, :Image)
+
+function image_manipulator(X)
+    l = []
+    for i ∈ ProgressBar(1:size(X, 1))
+        img = img_vals(X[i])
+        # push!(l, img)
+        append!(l, img)
+    end
+    reshape(l, 1, 96, 96, size(X, 1))
+end
+
+
+x = image_manipulator(X)
+
+
+function img_vals(x)
+    img = split(x)
+    img = tryparse.(Float64, img)
+    img = reshape(img, 96, 96)
+    img = img' # To fix the orientation of the images
+    img
+end
+
+test3 = reshape(x, 1, 96, 96, 7049)
+
+imshow(x[1, :, :, 2])
