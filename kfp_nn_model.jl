fe4bce964720ac7f132cf544616df046310e9667
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-07-21 11:44:43 -0600

    added doc strings and changed to float 32

--- kfp_nn_model.jl
+++ kfp_nn_model.jl
@@ -9,21 +9,30 @@ using CUDAnative
 using Statistics
 using ProgressBars
 using MLDataUtils
+using MLDataPattern
+
 
 train = CSV.read("data/training.csv") |> DataFrame;
+X = train[:Image];
+
+deletecols!(train, :Image);
 
 describe(train)
 ## Lots of missing values
-
-
-
+"""
+Calculates all missing values of a DF Col
+"""
 function missing_by_col(x)
     sum(ismissing.(x))
 end
 
 
-colwise(missing_by_col, train)
+[missing_by_col(col) for col = eachcol(train)]
 
+"""
+Replaces all missing values for a dataframe.
+N.B. This only works on numeric columns.
+"""
 function replace_missing(df)
     for col in names(df)
         col_mean = mean(skipmissing(df[col]))
@@ -32,41 +41,46 @@ function replace_missing(df)
     return df
 end
 
-trian = replace_missing(train)
-colwise(missing_by_col, train)
 
-## Fixed missing
-X = train[:Image];
+trian = replace_missing(train);
+[missing_by_col(col) for col = eachcol(train)] |> sum
 
-delete!(train, :Image);
+## Image Manipulator
 
+"""
+Creates list of image values.
+"""
 function image_manipulator(X)
     l = []
     for i ∈ ProgressBar(1:size(X, 1))
         img = img_vals(X[i])
-        # push!(l, img)
+        img = Float64.(img) # Might have to make it 32
         append!(l, img)
     end
-    reshape(l, 1, 96, 96, size(X, 1))
+    l = reshape(l, 1, 96, 96, size(X, 1))
+    Float64.(l)
 end
 
-
+"""
+Splits cell of a dataframe into an image when seperated by spaces.
+"""
 function img_vals(x)
     img = split(x)
     img = tryparse.(Float64, img)
     img = reshape(img, 96, 96)
     img = img' # To fix the orientation of the images
+    img = Float32.(img)
     img
 end
 
-x = image_manipulator(X)
+x = image_manipulator(X);
 
+imshow(x[1, :, :, 2])
 
-
-test3 = reshape(x, 1, 96, 96, 7049)
-
-imshow(x[1, :, :, 1])
-
-## TODO
 # Make train test split
+
+y_train, y_val = splitobs(train, 0.7);
+x_train, x_val = splitobs(x, 0.7);
+
+## Model
 
